4bccea6df44e2ca12d9fd4165111f8c6ec13436c
ecarcamo <estebancarcamou@gmail.com>
2025-03-31 17:55:21 -0600

    skills component

---
+++ src/data/skills.js
@@ -0,0 +1,41 @@
+const colorGris = "text-gris";
+
+export const skills = [
+  {
+    name: "Javascript",
+    bgColor: "bg-yellow-300",
+    textColor: colorGris,
+    icon: "/lenguajes/js-logo.png",
+  },
+  {
+    name: "Java",
+    bgColor: "bg-red-300",
+    textColor: colorGris,
+    icon: "/lenguajes/java-logo.png",
+  },
+  {
+    name: "Postgresql",
+    bgColor: "bg-blue-300",
+    textColor: colorGris,
+    icon: "/lenguajes/postgres-logo.png",
+  },
+  {
+    name: "Astro JS",
+    bgColor: "bg-orange-300",
+    textColor: colorGris,
+    icon: "/lenguajes/astrojs-logo.png",
+  },
+  {
+    name: "Python",
+    bgColor: "bg-yellow-200",
+    textColor: colorGris,
+    icon: "/lenguajes/python-logo.png",
+  },
+  {
+    name: "Kotlin",
+    bgColor: "bg-fuchsia-300",
+    textColor: colorGris,
+    icon: "/lenguajes/kotlin-logo.png",
+  },
+  // Agrega más elementos según necesites
+];
