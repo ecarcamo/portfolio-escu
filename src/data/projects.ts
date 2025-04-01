ddc3eb92566d95fdde26b76532f6faeda9cab301
ecarcamo <estebancarcamou@gmail.com>
2025-04-01 02:47:23 -0600

    section of projects and adjusting all de index

---
+++ src/data/projects.ts
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    nameProject: "Tag NFC Cheking",
+    imageDemo: "/projects/tag-nfc.png",
+    urlDemo: "https://jcarcamo.com/checkin/pruebaPrueba",
+    urlRepo: "https://github.com/csuvg/nfc_checkin",
+    description:
+      "Proyecto desarrollado para la experiencia UVG 2025 para el departamento de marketing donde buscaban guardar usuarios que fueran a visitar las actividades y poder guardar a los usuarios mediante el uso de tags NFC que se les entregaban en froma de llaveros",
+    languages: ["FastAPI", "Postgres"],
+  },
+  {
+    nameProject: "Interprete LISP",
+    imageDemo: "/projects/lisp-interprete.png",
+    urlDemo: "https://youtu.be/39DrBsivOtI?si=imPoLZ7v2k9u9T3U",
+    urlRepo: "https://github.com/CarEsteban/proyecto1-Datos",
+    description:
+      "Proyecto desarrollado para poder realizar un interprete del lenguaje LISP mediante el uso del lenguaje apropiado, el cual permite realizar un analisis lexicografico y sintactico del lenguaje LISP, ademas de poder realizar operaciones aritmeticas y logicas",
+    languages: ["Java"],
+  },
+  {
+    nameProject: "Visualizador Estructuras",
+    imageDemo: "/projects/visualizador-datos.png",
+    urlDemo: "https://youtu.be/JRHCfmGv61I?si=_5Mfbe7FXF6Qk13O",
+    urlRepo: "https://github.com/AscencioSIUU/DataStructureVisualizer-",
+    description:
+      "Aplicación Android desarrollada para poder realizar un visualizador de estructuras de datos, ademas de poder realizar operaciones aritmeticas y logicas",
+    languages: ["Kotlin"],
+  },
+  {
+    nameProject: "Imitacion Disney",
+    imageDemo: "/projects/imitacion-disney.png",
+    urlDemo: "none",
+    urlRepo: "none",
+    description:
+      "Página web de práctica imitando la página de Disney del año 2020",
+    languages: ["HTML", "CSS", "Javascript"],
+  },
+];
