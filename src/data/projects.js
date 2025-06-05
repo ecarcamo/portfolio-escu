0880dd0ee3dd5e2235184a92b64973abfc26d5ff
ecarcamo <estebancarcamou@gmail.com>
2025-06-04 20:38:42 -0600

    files in public

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,37 +2,37 @@ export default [
 
   {
     name: "Juego de memoria robusto con React",
-    image: "src/assets/projects/juego-memoria.png",
+    image: "/projects/juego-memoria.png",
     description: "Un juego de memoria robusto desarrollado con React con temática de Rick and Morty, que permite a los usuarios mejorar su memoria y concentración; construida con base al API oficial de Rick and Morty.",
     repoUrl: "https://github.com/CarEsteban/lab7_web"
   },
   {
     name: "Visualizador de Estructuras de Datos",
-    image: "src/assets/projects/visualizador-datos.png",
+    image: "/projects/visualizador-datos.png",
     description: "Applición movil enfocada para dispositivos Android que funcion para visualizar estructuras de datos como listas, pilas y colas con recolección de datos mediante un API, un archivo CSV o una entrada manual de datos.",
     repoUrl: "https://github.com/AscencioSIUU/DataStructureVisualizer-"
   },
   {
     name: "Chat en vivo",
-    image: "src/assets/projects/chat-en-vivo.png",
+    image: "/projects/chat-en-vivo.png",
     description: "Aplicación web de chat en vivo consumiendo un API externo que permite a los usuarios comunicarse en tiempo real, con una interfaz intuitiva y fácil de usar.",
     repoUrl: "https://github.com/CarEsteban/lab5_web"
   },
   {
     name: "Tag NFC",
-    image: "src/assets/projects/tag-nfc.png",
+    image: "/projects/tag-nfc.png",
     description: "Sistema de identificación y acceso utilizando etiquetas NFC para la Expo UVG 2025 en la cual el departamento de marketing necesitaba un sistema para manejar a sus usuarios.",
     repoUrl: "https://github.com/csuvg/nfc_checkin"
   },
   {
     name: "Lisp Intérprete",
-    image: "src/assets/projects/lisp-interprete.png",
+    image: "/projects/lisp-interprete.png",
     description: "Un intérprete de Lisp para experimentar con lenguajes de programación.",
     repoUrl: "https://github.com/CarEsteban/proyecto1-Datos"
   },
   {
     name: "Calculadora web",
-    image: "src/assets/projects/calculadora.png",
+    image: "/projects/calculadora.png",
     description: "Una calculadora web simple que permite realizar operaciones básicas mediante una interfaz amigable.",
     repoUrl: "https://github.com/CarEsteban/proyecto-calculadora"
   }
